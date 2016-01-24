dd964a4dd2e78b8197c2673decbb077a2c6824b0
nperiwal <narayan.periwal@inmobi.com>
2016-01-24 15:27:55 +0530

    Reverse 2048

--- src/com/uberspot/a2048/MainActivity.java
+++ src/com/uberspot/a2048/MainActivity.java
@@ -1,180 +1,136 @@
-
 package com.uberspot.a2048;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.WindowManager.LayoutParams;
-import android.webkit.WebSettings;
-import android.webkit.WebSettings.RenderPriority;
-import android.webkit.WebView;
-import android.widget.Toast;
-
-import de.cketti.library.changelog.ChangeLog;
+import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
 
-    private static final String MAIN_ACTIVITY_TAG = "2048_MainActivity";
+    private static final String WIDTH = "width";
+    private static final String HEIGHT = "height";
+    private static final String SCORE = "score";
+    private static final String HIGH_SCORE = "high score temp";
+    private static final String UNDO_SCORE = "undo score";
+    private static final String CAN_UNDO = "can undo";
+    private static final String UNDO_GRID = "undo";
+    private static final String GAME_STATE = "game state";
+    private static final String UNDO_GAME_STATE = "undo game state";
+    private MainView view;
 
-    private WebView mWebView;
-    private long mLastBackPress;
-    private static final long mBackPressThreshold = 3500;
-    private static final String IS_FULLSCREEN_PREF = "is_fullscreen_pref";
-    private static boolean DEF_FULLSCREEN = true;
-    private long mLastTouch;
-    private static final long mTouchThreshold = 2000;
-    private Toast pressBackToast;
-
-    @SuppressLint({ "SetJavaScriptEnabled", "NewApi", "ShowToast" })
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        view = new MainView(this);
 
-        // Don't show an action bar or title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        view.hasSaveState = settings.getBoolean("save_state", false);
 
-        // If on android 3.0+ activate hardware acceleration
-        if (Build.VERSION.SDK_INT >= 11) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-        }
-
-        // Apply previous setting about showing status bar or not
-        applyFullScreen(isFullScreen());
-
-        // Check if screen rotation is locked in settings
-        boolean isOrientationEnabled = false;
-        try {
-            isOrientationEnabled = Settings.System.getInt(getContentResolver(),
-                    Settings.System.ACCELEROMETER_ROTATION) == 1;
-        } catch (SettingNotFoundException e) {
-            Log.d(MAIN_ACTIVITY_TAG, "Settings could not be loaded");
-        }
-
-        // If rotation isn't locked and it's a LARGE screen then add orientation changes based on sensor
-        int screenLayout = getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK;
-        if (((screenLayout == Configuration.SCREENLAYOUT_SIZE_LARGE)
-                || (screenLayout == Configuration.SCREENLAYOUT_SIZE_XLARGE))
-                    && isOrientationEnabled) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-        }
-
-        setContentView(R.layout.activity_main);
-
-        ChangeLog cl = new ChangeLog(this);
-        if (cl.isFirstRun()) {
-            cl.getLogDialog().show();
-        }
-
-        // Load webview with game
-        mWebView = (WebView) findViewById(R.id.mainWebView);
-        WebSettings settings = mWebView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setRenderPriority(RenderPriority.HIGH);
-        settings.setDatabasePath(getFilesDir().getParentFile().getPath() + "/databases");
-
-        // If there is a previous instance restore it in the webview
         if (savedInstanceState != null) {
-            mWebView.restoreState(savedInstanceState);
-        } else {
-            mWebView.loadUrl("file:///android_asset/2048/index.html");
-        }
-
-        Toast.makeText(getApplication(), R.string.toggle_fullscreen, Toast.LENGTH_SHORT).show();
-        // Set fullscreen toggle on webview LongClick
-        mWebView.setOnTouchListener(new OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // Implement a long touch action by comparing
-                // time between action up and action down
-                long currentTime = System.currentTimeMillis();
-                if ((event.getAction() == MotionEvent.ACTION_UP)
-                        && (Math.abs(currentTime - mLastTouch) > mTouchThreshold)) {
-                    boolean toggledFullScreen = !isFullScreen();
-                    saveFullScreen(toggledFullScreen);
-                    applyFullScreen(toggledFullScreen);
-                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    mLastTouch = currentTime;
-                }
-                // return so that the event isn't consumed but used
-                // by the webview as well
-                return false;
+            if (savedInstanceState.getBoolean("hasState")) {
+                load();
             }
-        });
-
-        pressBackToast = Toast.makeText(getApplicationContext(), R.string.press_back_again_to_exit,
-                Toast.LENGTH_SHORT);
+        }
+        setContentView(view);
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        mWebView.saveState(outState);
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            //Do nothing
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+            view.game.move(2);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
+            view.game.move(0);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            view.game.move(3);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+            view.game.move(1);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("hasState", true);
+        save();
     }
 
-    private void saveFullScreen(boolean isFullScreen) {
-        // save in preferences
-        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-        editor.putBoolean(IS_FULLSCREEN_PREF, isFullScreen);
+    protected void onPause() {
+        super.onPause();
+        save();
+    }
+
+    private void save() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = settings.edit();
+        Tile[][] field = view.game.grid.field;
+        Tile[][] undoField = view.game.grid.undoField;
+        editor.putInt(WIDTH, field.length);
+        editor.putInt(HEIGHT, field.length);
+        for (int xx = 0; xx < field.length; xx++) {
+            for (int yy = 0; yy < field[0].length; yy++) {
+                if (field[xx][yy] != null) {
+                    editor.putInt(xx + " " + yy, field[xx][yy].getValue());
+                } else {
+                    editor.putInt(xx + " " + yy, 0);
+                }
+
+                if (undoField[xx][yy] != null) {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, undoField[xx][yy].getValue());
+                } else {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, 0);
+                }
+            }
+        }
+        editor.putLong(SCORE, view.game.score);
+        editor.putLong(HIGH_SCORE, view.game.highScore);
+        editor.putLong(UNDO_SCORE, view.game.lastScore);
+        editor.putBoolean(CAN_UNDO, view.game.canUndo);
+        editor.putInt(GAME_STATE, view.game.gameState);
+        editor.putInt(UNDO_GAME_STATE, view.game.lastGameState);
         editor.commit();
     }
 
-    private boolean isFullScreen() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean(IS_FULLSCREEN_PREF,
-                DEF_FULLSCREEN);
+    protected void onResume() {
+        super.onResume();
+        load();
     }
 
-    /**
-     * Toggles the activitys fullscreen mode by setting the corresponding window flag
-     * @param isFullScreen
-     */
-    private void applyFullScreen(boolean isFullScreen) {
-        if (isFullScreen) {
-            getWindow().clearFlags(LayoutParams.FLAG_FULLSCREEN);
-        } else {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    private void load() {
+        //Stopping all animations
+        view.game.aGrid.cancelAnimations();
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        for (int xx = 0; xx < view.game.grid.field.length; xx++) {
+            for (int yy = 0; yy < view.game.grid.field[0].length; yy++) {
+                int value = settings.getInt(xx + " " + yy, -1);
+                if (value > 0) {
+                    view.game.grid.field[xx][yy] = new Tile(xx, yy, value);
+                } else if (value == 0) {
+                    view.game.grid.field[xx][yy] = null;
+                }
+
+                int undoValue = settings.getInt(UNDO_GRID + xx + " " + yy, -1);
+                if (undoValue > 0) {
+                    view.game.grid.undoField[xx][yy] = new Tile(xx, yy, undoValue);
+                } else if (value == 0) {
+                    view.game.grid.undoField[xx][yy] = null;
+                }
+            }
         }
-    }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public void onBackPressed() {
-        long currentTime = System.currentTimeMillis();
-        if (Math.abs(currentTime - mLastBackPress) > mBackPressThreshold) {
-            pressBackToast.show();
-            mLastBackPress = currentTime;
-        } else {
-            pressBackToast.cancel();
-            super.onBackPressed();
-        }
+        view.game.score = settings.getLong(SCORE, view.game.score);
+        view.game.highScore = settings.getLong(HIGH_SCORE, view.game.highScore);
+        view.game.lastScore = settings.getLong(UNDO_SCORE, view.game.lastScore);
+        view.game.canUndo = settings.getBoolean(CAN_UNDO, view.game.canUndo);
+        view.game.gameState = settings.getInt(GAME_STATE, view.game.gameState);
+        view.game.lastGameState = settings.getInt(UNDO_GAME_STATE, view.game.lastGameState);
     }
 }
